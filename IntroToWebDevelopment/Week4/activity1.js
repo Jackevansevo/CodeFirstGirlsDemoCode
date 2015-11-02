c1dcce40cea59a750e8c3de5e1c23249e100d8cb
Jack Evans <jack@evans.gb.net>
2015-11-02 15:38:33 +0000

    Added activity 1

---
+++ IntroToWebDevelopment/Week4/activity1.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $(".btn-default").click(function() {
+        $("h1").css("color", "black");
+    });
+
+    $(".btn-primary").click(function() {
+        $("h1").css("color", "blue");
+    });
+
+    $(".btn-success").click(function() {
+        $("h1").css("color", "green");
+    });
+
+    $(".btn-info").click(function() {
+        $("h1").css("color", "turquoise");
+    });
+
+    $(".btn-warning").click(function() {
+        $("h1").css("color", "orange");
+    });
+
+    $(".btn-danger").click(function() {
+        $("h1").css("color", "red");
+    });
+
+});
