6837bc39eac521b507dc9a562c5e3d47ec912e4a
Jack Evans <jack@evans.gb.net>
2015-11-02 16:20:08 +0000

    Updated activity 1

--- IntroToWebDevelopment/Week4/activity1.js
+++ IntroToWebDevelopment/Week4/activity1.js
@@ -1,27 +1,13 @@
 $(document).ready(function() {
 
+    // btn-default turns the font black
     $(".btn-default").click(function() {
         $("h1").css("color", "black");
     });
 
-    $(".btn-primary").click(function() {
-        $("h1").css("color", "blue");
-    });
+    // TASK: Add more on click events to style the header for the rest of the
+    // buttons
 
-    $(".btn-success").click(function() {
-        $("h1").css("color", "green");
-    });
-
-    $(".btn-info").click(function() {
-        $("h1").css("color", "turquoise");
-    });
-
-    $(".btn-warning").click(function() {
-        $("h1").css("color", "orange");
-    });
-
-    $(".btn-danger").click(function() {
-        $("h1").css("color", "red");
-    });
-
+    // EXTRA TASK: Implement reset button functionality, google how to make
+    // font underlined/bold/itallic with css
 });
