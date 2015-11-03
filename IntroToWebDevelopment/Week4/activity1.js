b98db841d21729dc30e1b00bf93b8f415abac65e
Jack Evans <jack@evans.gb.net>
2015-11-03 17:13:55 +0000

    Updated activity1 instructions

--- IntroToWebDevelopment/Week4/activity1.js
+++ IntroToWebDevelopment/Week4/activity1.js
@@ -1,18 +1,21 @@
-// Add javascript code here
+// You should see this message appear in the console when you first load the
+// page, this line can be uncommented
 console.log("hello world");
 
-// Make additional functions to subtract, multiply and divide two numbers
-
-// Remember function syntax:
+// Recall the syntax for making functions
 function sumNumbers(x, y) {
     return x + y;
 }
 
-//Uncomment the below code to check that the functions work
-// console.log(sumNumbers(10, 11));
-// console.log(subtractNumbers(8, 2));
-// console.log(multiplyNumbers(5, 4));
-// console.log(divideNumbers(20, 2));
+// TASK:
+// Make additional functions to subtract, multiply and divide two numbers
+
+// Uncomment the below code to check that the functions work
+
+console.log(sumNumbers(10, 11));
+console.log(subtractNumbers(8, 2));
+console.log(multiplyNumbers(5, 4));
+console.log(divideNumbers(20, 2));
 
 // We can pass the output of one function to another, uncomment the line below to see
 // console.log(multiplyNumbers(sumNumbers(2, 2), sumNumbers(5, 3)));
