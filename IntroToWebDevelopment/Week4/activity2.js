30f9b622ada7f3a058b33a0b7a19dd617bbe3408
Jack Evans <jack@evans.gb.net>
2015-11-03 15:37:29 +0000

    Removed solutions for activity2.js

--- IntroToWebDevelopment/Week4/activity2.js
+++ IntroToWebDevelopment/Week4/activity2.js
@@ -10,46 +10,4 @@ $(document).ready(function() {
 
     // EXTRA TASK: Implement reset button functionality, google how to make
     // font underlined/bold/itallic with css
-    
-    $(".btn-primary").click(function() {
-        $("h1").css("color", "blue");
-    });
-    
-    $(".btn-success").click(function() {
-        $("h1").css("color", "green");
-    });
-    
-    $(".btn-info").click(function() {
-        $("h1").css("color", "turquoise");
-    });
-    
-    $(".btn-warning").click(function() {
-        $("h1").css("color", "orange");
-    });
-    
-    $(".btn-danger").click(function() {
-        $("h1").css("color", "red");
-    });
-    
-    $("#underline-btn").click(function() {
-        $("h1").css("text-decoration", "underline");
-    });
-    
-    $("#bold-btn").click(function() {
-        $("h1").css("font-weight", "bold");
-    });
-    
-    $("#italic-btn").click(function() {
-        $("h1").css("font-style", "italic");
-    });
-    
-    
-    $("#reset-btn").click(function() {
-        $("h1").css({
-            "text-decoration": "none",
-            "font-weight": "normal",
-            "font-style": "normal",
-            "color": "black"
-        });
-    });
 });
